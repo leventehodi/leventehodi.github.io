f233d7751a330d740a6657ae6f9bed9b857bb791
Levente <69513672+f1oppa@users.noreply.github.com>
2022-11-01 09:43:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function opendiscord(){
+    window.open("https://discord.com/users/877778571748331561/")
+}
+
+function opengithub(){
+    window.open("https://github.com/f1oppa/")
+}
+
+function opentelegram(){
+    window.open("https://t.me/f10ppa/")
+}
